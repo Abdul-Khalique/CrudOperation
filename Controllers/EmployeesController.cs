efdc4f6e65e40bee8b54b597a08c441e3c4f3894
Abdul-Khalique <khalique.abdul247@gmail.com>
2023-09-15 22:30:15 +0530

    Importing Files

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeID,Name,Email,Address,Mobile")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+        [HttpPost]
+        public ActionResult Createe(FormCollection formCollection)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee a = new Employee();
+                 a.Name = (formCollection["name"]);
+                 a.Email = (formCollection["email"]);
+                 a.Address = (formCollection["address"]);
+                 a.Mobile = (formCollection["phone"]);
+                db.Employees.Add(a);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+        public JsonResult EditEmployee(int? id)
+        {
+            Employee employee = db.Employees.Find(id);
+            return Json(employee,JsonRequestBehavior.AllowGet);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeID,Name,Email,Address,Mobile")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Editt(FormCollection formCollection)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee a = new Employee();
+                a.EmployeeID = Convert.ToInt32(formCollection["Id"]);
+                a.Name = (formCollection["name"]);
+                a.Email = (formCollection["email"]);
+                a.Address = (formCollection["address"]);
+                a.Mobile = (formCollection["phone"]);
+
+                db.Entry(a).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+        public JsonResult DeleteEmployee(int? id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return Json(employee, JsonRequestBehavior.AllowGet);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext()
+           : base("EmployeeDB")
+        { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
